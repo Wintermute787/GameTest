06ee41903775175a144cf26e5250348a7c31566e
Joe Barnes <joebarnes333@gmail.com>
2019-05-22 06:50:57 -0700

    add files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GameTest.Models;
+
+namespace GameTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/game")]
+        public ActionResult game2()
+        {
+            return View();
+        }
+
+      
+    }
+}
